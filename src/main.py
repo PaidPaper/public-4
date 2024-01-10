5205b35e6e9933ea345f8ad2f4623dc352c05d1a
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2024-01-10 16:30:37 +0500

    Apply suggestions from code review
    Co-authored-by: Henry R. <87513666+henry-richard@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@
 This is the main script for the script.
 """
 if __name__ == "__main__":
-    print("Hello world")
-    print("Hello worldssss")
+    print("Hello worlds")
+    print("Hello worldsssss")
