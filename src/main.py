f1506c6f46b0fc14820332c4f939b974eca52533
Henry R <87513666+henry-richard@users.noreply.github.com>
2024-05-01 23:25:28 +0500

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -13,4 +13,6 @@ if __name__ == "__main__":
     print("Hello worldsssss")
     print("Hello worldsssss")
     print("Hello worldsssss")
+    print("Hello worldsssss")
+    print("Hello worldsssss")
 
