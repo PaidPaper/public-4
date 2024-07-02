0272008620e23d256951469c1eed568e6732b03b
Henry R <87513666+henry-richard@users.noreply.github.com>
2024-07-02 21:31:02 +0500

    Update wanda.py

--- src/wanda.py
+++ src/wanda.py
@@ -16,4 +16,5 @@ print("Wanda testing preflight 0.8.1")
 print("Wanda testing preflight 0.8.1")
 print("Wanda testing preflight 0.8.1")
 print("Wanda testing preflight 0.8.1")
+print("Wanda testing preflight 0.8.1")
 
