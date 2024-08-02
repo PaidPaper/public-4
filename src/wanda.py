7dfc25d3ebc8334e504b6c5786eaa278a837a225
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2024-08-02 17:07:07 +0500

    Update src/wanda.py
    Co-authored-by: uma-paidpaper <124864660+uma-paidpaper@users.noreply.github.com>

--- src/wanda.py
+++ src/wanda.py
@@ -27,4 +27,5 @@ print("Wanda testing preflight 0.8.1")
 print("Wanda testing preflight 0.8.1")
 print("Wanda testing preflight 0.8.1")
 print("Henry testing v0.8.0")
+print("Henry testing v0.8.0")
 
