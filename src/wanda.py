8d6d7a210129a2efde1fc7c85e5aa8430c25c4e1
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2024-08-02 16:15:15 +0500

    Update wanda.py

--- src/wanda.py
+++ src/wanda.py
@@ -26,4 +26,5 @@ print("Wanda testing preflight 0.8.1")
 print("Wanda testing preflight 0.8.1")
 print("Wanda testing preflight 0.8.1")
 print("Wanda testing preflight 0.8.1")
+print("Henry testing v0.8.0")
 
