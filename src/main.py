7820d883c8769e52056dc454f4682d30f50f0f94
Henry R. <87513666+henry-richard@users.noreply.github.com>
2024-11-07 08:24:29 +0500

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -24,5 +24,5 @@ if __name__ == "__main__":
     print("Hello worldsssss")
     print("Hello worldsssss")
     print("Hello worldsssss")
-    print("Hello worldsssss")
+    print("Hello worldly beings")
 
